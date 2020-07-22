0d4a48528f7d75caf984b4b1ce17c83e7100565f
Koras02 <67436032+Koras02@users.noreply.github.com>
2020-07-22 23:46:38 +0900

    Delete update_process.php

--- update_process.php
+++
@@ -1,5 +0,0 @@
-<?php
-rename('data/'.$_POST['old_title'], 'data/'.$_POST['title']);
-file_put_contents('data/'.$_POST['title'], $_POST['description']);
-header('Location: /index.php?id='.$POST['title']);
-?>
